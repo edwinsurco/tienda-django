fffd235ff71fcd7e16ea5980d6cbe143b3e4ec8d
edwinsurco <edwinssurco2003@gmail.com>
2026-04-16 15:56:17 -0500

    Configurar STATIC_ROOT

--- tienda/settings.py
+++ tienda/settings.py
@@ -119,11 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-import os
-
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
