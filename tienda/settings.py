9032fa3c880173eebeb17b00c4bddb4441aa2560
edwinsurco <edwinssurco2003@gmail.com>
2026-05-06 18:29:56 -0500

    Agregar recuperar contraseña por correo

--- tienda/settings.py
+++ tienda/settings.py
@@ -143,3 +143,16 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'Autodisur@gmail.com'
+EMAIL_HOST_PASSWORD = 'cazd fiou pszf zyrq'
--- tienda/urls.py
+++ tienda/urls.py
@@ -6,10 +6,43 @@ from django.conf.urls.static import static
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('productos.urls')),
+
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='productos/password_reset.html'
+        ),
+        name='password_reset'
+    ),
+
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='productos/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='productos/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='productos/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
