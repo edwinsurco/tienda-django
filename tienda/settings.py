479717a7afdea7d4b9a338ec8cdbe948a052fadc
edwinsurco <edwinssurco2003@gmail.com>
2026-05-08 09:23:58 -0500

    Cambiar puerto SMTP Brevo

--- tienda/settings.py
+++ tienda/settings.py
@@ -151,7 +151,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
